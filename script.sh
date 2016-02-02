e9569cf4d49c340430aab526a8386148e663a5bb
Drew Sancho <drew_sancho@yahoo.com>
2016-02-02 14:22:46 -0500

    Initial Commit

---
+++ script.sh
@@ -0,0 +1,15 @@
+mkdir assignment
+
+cd assignment
+
+touch README index.html main.js main.css deletme.txt
+
+mkdir assets
+
+mv main.js main.css assets
+
+rm -r deletme.txt
+
+echo 'The command line rules!' > index.html
+
+ls -la > README
